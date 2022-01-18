70610f9e4229cf3b207c86757ff277a39d4281f4
Matt Swartley <mswartley@chariotsolutions.com>
2022-01-17 20:46:43 -0500

    Adjusted the positioning of the navigation bar.

--- hello-world-react/src/App.js
+++ hello-world-react/src/App.js
@@ -3,14 +3,16 @@ import { NavLink, Outlet } from "react-router-dom";
 const App = () => {
     return (
         <main className="mt-4 text-gray-900 mx-4 my-4 max-w-5xl lg:mx-auto">
-            <h1 className="text-4xl font-bold">Hello React!</h1>
-            <nav className="mt-4 space-x-6">
-                <AppNavLink to="/">Home</AppNavLink>
-                <AppNavLink to="/simple">Simple Components</AppNavLink>
-                <AppNavLink to="/jokes">Jokes</AppNavLink>
-                <AppNavLink to="/forms">Forms</AppNavLink>
-                <AppNavLink to="/dialogs">Dialogs</AppNavLink>
-            </nav>
+            <div className="flex items-center">
+                <h1 className="text-4xl font-bold">Hello React!</h1>
+                <nav className="ml-8 space-x-6">
+                    <AppNavLink to="/">Home</AppNavLink>
+                    <AppNavLink to="/simple">Simple Components</AppNavLink>
+                    <AppNavLink to="/jokes">Jokes</AppNavLink>
+                    <AppNavLink to="/forms">Forms</AppNavLink>
+                    <AppNavLink to="/dialogs">Dialogs</AppNavLink>
+                </nav>
+            </div>
             <div className="mt-5">
                 <Outlet/>
             </div>
