cf0f10b13de2b2382230642bf0de6f509f456fa3
Matt Swartley <mswartley@chariotsolutions.com>
2022-02-13 13:05:39 -0500

    Fixed references to "React" in the VueJS UI.

--- hello-world-vue/src/routes/AboutPage.vue
+++ hello-world-vue/src/routes/AboutPage.vue
@@ -1,12 +1,12 @@
 <template>
   <main class="mt-4 text-gray-900 mx-4 my-4 max-w-5xl lg:mx-auto">
     <tile>
-      <h3 class="text-xl font-normal">About Hello React!</h3>
+      <h3 class="text-xl font-normal">About Hello VueJS!</h3>
       <p class="mt-1 text-md text-gray-700 font-light">
-        This app contains components that illustrate everything that I have learned about React so far.
+        This app contains components that illustrate everything that I have learned about VueJS so far.
       </p>
       <p class="mt-1 text-md text-gray-700 font-light">
-        The use of React Router allows you to go back via this
+        The use of Vue-Router allows you to go back via this
         <router-link to="/" class="text-blue-500 border-blue-400 hover:border-b">link to the home page</router-link>,
         <i>or</i> by using the back button of your browser.
       </p>
